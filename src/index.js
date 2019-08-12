6fdc2e219d4980a26e5b73cc7428861f272d0080
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-08-12 08:53:02 +0200

    added in jsx status of game

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createSecureContext } from 'tls';
 
 function Square(props) {
     return (
@@ -79,10 +80,13 @@ class Game extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board 
+                        squares = {current.squares}
+                        onClick = {(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{status}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
