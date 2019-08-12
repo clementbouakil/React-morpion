e7b384849faf325c2b7a28bb3f9e657767844c72
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-08-12 08:48:39 +0200

    delete constructor from board

--- src/index.js
+++ src/index.js
@@ -11,14 +11,6 @@ function Square(props) {
 }
   
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNest: true,
-        };
-    }
-
     handleClick(i) {
         const squares = this.state.squares.slice();
         if(calculateWinner(squares) || squares[i]) {
